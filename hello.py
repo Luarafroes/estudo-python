65076eaf340b6474796e2dd789f524ab4e9280a3
Luara Froes <luara0511f@gmail.com>
2025-03-18 21:25:00 +0000

    isso e tudo q eu aprendi

--- hello.py
+++ hello.py
@@ -38,10 +38,47 @@
 #print(-6//4)
 #print(6.//-4)
 #Adiós_Señora = 3
-var = 1
-account_balance = 1000.0
-client_name = 'John Doe'
-print(var, account_balance, client_name)
-print(var)
-var = 2
-print (var)
+#var = 1
+#account_balance = 1000.0
+#client_name = 'John Doe'
+#print(var, account_balance, client_name)
+#print(var)
+#var = 2
+#print (var)
+#var = '3.8.5'
+#print ("python version:" + var)
+#print ("Luara" + account_balance)
+#var = 100
+#print(var)
+#var = 200 + 300
+#print(var+var)
+#a = 3.0
+#b = 4.0 
+#c = (a**2 + b**2)**0.5
+#print(c)
+#johns_apples = 3
+#marys_apples = 5
+#Adans_apples = 6
+#print(johns_apples, marys_apples, Adans_apples, sep=",") 
+#total_apples = johns_apples+marys_apples+Adans_apples
+#print (total_apples)
+#print("total number of apples:" , total_apples)
+#i= 1
+#var= 2
+#rem= 3
+#j = 2
+#x = 1
+#i += 2*j
+#var /= 2
+#rem %= 10
+#j-=(i+var+rem)
+#x **=2
+#print (i, j, var, rem, x)
+#print (i+j)
+'''kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = miles*1.61
+kilometers_to_miles = kilometers/ 1.61
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+print("bla bla bla")''' #voce pode comentar assim usando hashtag ou usando os 3 pontinhos um no comeco e outro no final
