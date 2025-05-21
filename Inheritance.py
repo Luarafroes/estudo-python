cc71836669045952eff9146620da5bd0e88a5dfb
Luarafroes <luara0511f@gmail.com>
2025-05-21 12:27:31 +0100

    inheritance

--- Inheritance.py
+++ Inheritance.py
@@ -1,5 +1,20 @@
 class Music: #this is the parent class
-    pass
+    counter = 0 # this is variable of the class and it will be shared by all the instances of the class
+    def __init__(self, Artist, SongName, popularity):
+        self.Artist = Artist
+        self.SongName = SongName
+        self.popularity = popularity
+        Music.counter += 1
+    def set_mycomment(self, comment):
+        self.coment = comment
+        
+M1 = Music("Rose", "APT", "really popular")
+M2 = Music("A-ha", "Take on me", "really popular")
+M1.set_mycomment("I love this song, but I'm a bit tired of it")
+M2.set_mycomment("this song is really good, thaks Lucas for the recommendation")
+print(M1.__dict__, M1.counter) # this will print the dictionary of the object M1
+
+
 class Kpop(Music): #this is the child class of Music
     pass
 class Groups(Kpop): 
@@ -28,3 +43,4 @@ class BringMeTheHorizon(band):
 
 class LinkinPark(band):
     pass
+
